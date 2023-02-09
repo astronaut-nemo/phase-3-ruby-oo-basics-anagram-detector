641428505f5c14a14f253527daab055de69ed5fb
Joanna <astronautnemo@gmail.com>
2023-02-09 10:55:28 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_array)
+        # Use #select to return an array of all matches or an empty array for no matches.
+        word_array.select{|arr_word| arr_word.chars.sort == @word.chars.sort}
+    end
+end
